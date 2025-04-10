3014329c2455d6c77e80f573e09ce86e2feba2bc
Matthias Bloch <matbloch@users.noreply.github.com>
2025-04-10 10:04:29 +0200

    update index

--- circular_action_progress/frontend/src/index.tsx
+++ circular_action_progress/frontend/src/index.tsx
@@ -1,10 +1,13 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import ReactDOM from "react-dom/client"
 import CircularProgress from "./CircularProgress"
 
-ReactDOM.render(
+const rootElement = document.getElementById("root")
+if (!rootElement) throw new Error("Failed to find the root element")
+
+const root = ReactDOM.createRoot(rootElement)
+root.render(
   <React.StrictMode>
     <CircularProgress/>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 )
